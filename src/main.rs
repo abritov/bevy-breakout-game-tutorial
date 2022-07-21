82b639fc06451cc3b01ac3933f37bb60d51033fc
envy124 <pkgsail.dev@gmail.com>
2022-07-21 17:04:16 +0300

    change window title

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use bevy::{
 
 fn main() {
     App::new()
+        .insert_resource(WindowDescriptor {
+            title: "Rust & Bevy Breakout game!".to_string(),
+            ..default()
+        })
         .add_plugins(DefaultPlugins)
         .add_system(bevy::input::system::exit_on_esc_system)
         .run();
