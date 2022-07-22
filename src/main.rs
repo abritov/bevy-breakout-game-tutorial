70c2c589bd0e2dce709193d3e6ca30242a106fd7
envy124 <pkgsail.dev@gmail.com>
2022-07-22 10:00:41 +0300

    add initial_setup system

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ use bevy::{
     prelude::*,
 };
 
+fn initial_setup(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(UiCameraBundle::default());
+}
+
 fn main() {
     App::new()
         .insert_resource(WindowDescriptor {
@@ -9,6 +14,7 @@ fn main() {
             ..default()
         })
         .add_plugins(DefaultPlugins)
+        .add_startup_system(initial_setup)
         .add_system(bevy::input::system::exit_on_esc_system)
         .run();
 }
